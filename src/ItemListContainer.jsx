f89c6c2571cf016d9d9b09529da6814c9afd0857
Daniel Gamboa <d.2610@hotmail.com>
2021-07-01 21:22:35 -0500

    Se crea ItemCount

--- src/ItemListContainer.jsx
+++ src/ItemListContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ItemCount from './components/ItemCount'
 
 function ItemListContainer ( {greeting}) {
     return (
         <div>
             <h2>{greeting}</h2>
+            <ItemCount/>
+
         </div>
     )
 }
---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+
+let stock = 10
+let cantidadMinima = 1
+
+export const ItemCount = () => {
+    const [contador, setContador] = useState(1)
+
+    function disminucion() {
+        if (contador > cantidadMinima) {
+            setContador(contador-1)
+        };
+        if (contador === cantidadMinima)
+        alert ('No se puede agregar menos de un producto')
+    };
+
+    function incremento() {
+        if (contador < stock) {
+            setContador(contador+1)
+        };
+        if (contador === stock)
+        alert ('No tenemos mas unides disponibles de este producto')
+    };
+       
+    return(
+        <>
+        <section>
+            <button onClick={() =>{disminucion()}}>-</button>
+            {contador}
+            <button onClick={() =>{incremento()}}>+</button>
+        </section>
+        <section>
+            <button>Agregar al Carrito</button>
+        </section>
+        </>
+    )
+}
+
+export default ItemCount
